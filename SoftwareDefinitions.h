1b72a4bf56ca663dbf191873ff2df5796c0c84e1
Quality <qwerty15092008@gmail.com>
2022-05-15 16:10:41 +0300

    Применение шрифта и подключение gdiplus

--- SoftwareDefinitions.h
+++ SoftwareDefinitions.h
@@ -4,10 +4,38 @@
 #define OnHelpClicked		1
 #define OnDownloadClicked	2
 #define OnOpenClicked		3
+#define OnDonateClicked		4
+#define OnExitClicked		5
+
+#define DownloadBtnWidth	200
+#define DownloadBtnHeight	60
+
+#define DonateBtnWidth	150
+#define DonateBtnHeight	50
+
+#define ExitBtnWidth	100
+#define ExitBtnHeight	50
+
+#define HelpBtnWidth	100
+#define HelpBtnHeight	50
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 TCHAR WinName[] = _T("MainFrame");
 
+RECT wndRect;
+RECT fontRect;
+PAINTSTRUCT ps;
+HBRUSH hBrush;
+HFONT hFont;
+HFONT hDefaultFont;
+HDC hdc;
+
+HWND hDownload;
+HWND hHelp;
+HWND hDonate;
+HWND hExit;
+
 void MainWndAddMenus(HWND hWnd);
 void MainWndVidgetes(HWND hWnd);
-void loadImage();
+void draw(HDC hdc);
+void FontApply();
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <tchar.h>
 #include "keys.h"
 #include "SoftwareDefinitions.h"
+#include <gdiplus.h>
 
 #pragma comment(linker,"\"/manifestdependency:type='win32' \
 name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
@@ -12,6 +13,9 @@ int WINAPI _tWinMain(HINSTANCE This, // Дескриптор текущего приложения
 	LPTSTR cmd, // Командная строка
 	int mode) // Режим отображения окна
 {
+	Gdiplus::GdiplusStartupInput gdiplusStartInput;
+	UINT_PTR gdiplusToken;
+	Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartInput, nullptr);
 	HWND hWnd; // Дескриптор главного окна программы
 	MSG msg; // Структура для хранения сообщения
 	WNDCLASS wc; // Класс окна
@@ -40,6 +44,7 @@ int WINAPI _tWinMain(HINSTANCE This, // Дескриптор текущего приложения
 		NULL, // Нет меню
 		This, // Дескриптор приложения
 		NULL); // Дополнительной информации нет
+	
 	ShowWindow(hWnd, mode); //Показать окно
 	// Цикл обработки сообщений
 	while (GetMessage(&msg, NULL, 0, 0))
@@ -47,6 +52,8 @@ int WINAPI _tWinMain(HINSTANCE This, // Дескриптор текущего приложения
 		TranslateMessage(&msg);// Функция трансляции кодов нажатой клавиши
 		DispatchMessage(&msg); // Посылает сообщение функции WndProc()
 	}
+
+	Gdiplus::GdiplusShutdown(gdiplusToken);
 	return 0;
 };
 
@@ -55,11 +62,30 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
 {
 	switch (message)
 	{
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		
+		hBrush = CreateSolidBrush(RGB(240, 128, 128));
+		FillRect(ps.hdc, &wndRect, hBrush);
+		
+		RedrawWindow(hWnd, NULL, NULL, RDW_UPDATENOW);
+
+		SetBkMode(ps.hdc, TRANSPARENT);
+		SetTextColor(ps.hdc, RGB(255, 255, 255));
+		SelectObject(ps.hdc, hFont);
+		DrawTextA(ps.hdc, "Dota 2 cheats installer", -1, &fontRect, DT_SINGLELINE | DT_CENTER | DT_VCENTER | DT_NOCLIP);
+
+		draw(hdc);
+
+		EndPaint(hWnd, &ps);
+		break;
+
 	case WM_CREATE:
-		//loadImage();
 		MainWndAddMenus(hWnd);
 		MainWndVidgetes(hWnd);
+		FontApply();
 		break;
+
 	case WM_COMMAND:
 		switch (wParam) {
 		case OnHelpClicked:
@@ -69,15 +95,22 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
 			SetWindowLongPtr(hWnd, GWL_STYLE, WS_POPUP | WS_VISIBLE);
 			SetWindowPos(hWnd, HWND_TOP, 0, 0, 1920, 1080, SWP_SHOWWINDOW);
 			break;
-		case OnOpenClicked:
+		case OnDonateClicked:
+			MessageBox(hWnd, L"Немедленно верните маме её банковскую карту!", L"Нельзя так", MB_ICONERROR);
 			break;
+		case OnOpenClicked:
+			ShellExecute(hWnd, NULL, L"C:\\Users\\qwert\\Desktop\\Xily.bat", NULL, NULL, SW_SHOW); // Открытие файла
+			break;
+		case OnExitClicked:
+			PostQuitMessage(0);
+			break;
+
 		default: break; }
 		break;
-	case WM_KEYDOWN:
-		if (wParam == K_L) PostQuitMessage(0);
-		break;
+
 	case WM_DESTROY: PostQuitMessage(0);
 		break;
+
 	default: return DefWindowProc(hWnd, message, wParam, lParam);
 	}
 	return 0;
@@ -87,20 +120,49 @@ void MainWndAddMenus(HWND hWnd)
 {
 	HMENU RootMenu = CreateMenu();
 
-	AppendMenu(RootMenu, MF_STRING, OnHelpClicked, L"Help");
+	// AppendMenu(RootMenu, MF_STRING, OnHelpClicked, L"Help");
 	AppendMenu(RootMenu, MF_STRING, OnOpenClicked, L"Open");
+	// AppendMenu(RootMenu, MF_STRING, OnExitClicked, L"Exit");
 
 	SetMenu(hWnd, RootMenu);
 }
 
 void MainWndVidgetes(HWND hWnd)
 {
-	CreateWindow(L"static", L"Static window", WS_VISIBLE | WS_CHILD | ES_CENTER, 5, 5, 100, 30, hWnd, NULL, NULL, NULL);
+	GetClientRect(hWnd, &wndRect);
 
-	CreateWindow(L"button", L"Download!", WS_VISIBLE | WS_CHILD | ES_CENTER, 5, 5, 100, 30, hWnd, (HMENU)OnDownloadClicked, NULL, NULL);
+	hFont = CreateFont(
+		80, 30, 0, 0, FW_THIN, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_OUTLINE_PRECIS, CLIP_DEFAULT_PRECIS,
+		ANTIALIASED_QUALITY, FF_DONTCARE, L"myFont");
+
+	hDefaultFont = (HFONT)GetStockObject(DEFAULT_GUI_FONT);
+
+	fontRect = { 220, 10, 500, 90 };
+
+	hDownload = CreateWindow(L"button", L"Устоновить!", WS_VISIBLE | WS_CHILD | ES_CENTER, 
+		wndRect.left + 10, wndRect.bottom - DownloadBtnHeight - 10, DownloadBtnWidth, DownloadBtnHeight, hWnd, (HMENU)OnDownloadClicked, NULL, NULL);
+	hDonate = CreateWindow(L"button", L"Поддержать автора", WS_VISIBLE | WS_CHILD | ES_CENTER, 
+		wndRect.left + 10, wndRect.bottom - DonateBtnHeight - 80, DonateBtnWidth, DonateBtnHeight, hWnd, (HMENU)OnDonateClicked, NULL, NULL);
+	hExit = CreateWindow(L"button", L"Выйти", WS_VISIBLE | WS_CHILD | ES_CENTER, 
+		wndRect.right - ExitBtnWidth - 10, wndRect.top + 10, ExitBtnWidth, ExitBtnHeight, hWnd, (HMENU)OnExitClicked, NULL, NULL);
+	hHelp = CreateWindow(L"button", L"Помощь", WS_VISIBLE | WS_CHILD | ES_CENTER,
+		wndRect.right - HelpBtnWidth - 10, wndRect.top + 70, HelpBtnWidth, HelpBtnHeight, hWnd, (HMENU)OnHelpClicked, NULL, NULL);
 }
 
-void loadImage()
+void draw(HDC hdc) 
 {
-	//hLogoImage = (HBITMAP)LoadImageW(NULL, L"logo.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+	Gdiplus::Graphics gf(hdc);
+	Gdiplus::Pen pen(Gdiplus::Color(255, 255, 0, 0));
+	Gdiplus::SolidBrush brush(Gdiplus::Color(255, 0, 255, 0));
+
+	gf.DrawLine(&pen, 0, 0, 500, 500); // Drawing line
+	gf.FillRectangle(&brush, 400, 100, 200, 300);
+}
+
+void FontApply()
+{
+	SendMessage(hExit, WM_SETFONT, (WPARAM)hDefaultFont, TRUE);
+	SendMessage(hDonate, WM_SETFONT, (WPARAM)hDefaultFont, TRUE);
+	SendMessage(hDownload, WM_SETFONT, (WPARAM)hDefaultFont, TRUE);
+	SendMessage(hHelp, WM_SETFONT, (WPARAM)hDefaultFont, TRUE);
 }
